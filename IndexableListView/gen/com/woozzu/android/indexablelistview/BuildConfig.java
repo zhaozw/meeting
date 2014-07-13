4f7275eaee5c296bd375ca42652bba73e391b860
shz <shz_gy@sina.cn>
2014-07-13 23:23:31 +0800

    add IndexableListView

---
+++ IndexableListView/gen/com/woozzu/android/indexablelistview/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.woozzu.android.indexablelistview;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
